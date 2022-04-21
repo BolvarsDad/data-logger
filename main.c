e78629b57d917e08f1cc1f01b75bf741c9261161
Bolvarsdad <sinan.p.pasic@gmail.com>
2022-04-21 18:38:56 +0200

    message

--- main.c
+++ main.c
@@ -24,8 +24,8 @@ strtou64(char const *buffer, size_t bufsz)
     return out;
 }
 
-int
-take_user_input(char const *prompt)
+uint64_t
+take_user_input(void)
 {
     char buffer[USERINPUT_MAX];
     size_t buflen;
@@ -41,53 +41,60 @@ take_user_input(char const *prompt)
     return strtou64(buffer, buflen); 
 }
 
+// this is faster than sorting (O(n) vs (O(n log n)))
 struct lohistat
-get_lohi(int *array, size_t arrayLen)
+compute_lohi(int *array, size_t arrayLen)
 {
     struct lohistat stat = {0, 0};
 
-    for (int i = 0; i < arrayLen; ++i)
+    for (size_t i = 0; i < arrayLen; ++i)
     {
-        for (int i = 0; i < arrayLen; ++i)
-            if (array[i] < array[stat.low])
-                stat.low = i;
+        if (array[i] < array[stat.low])
+            stat.low = i;
 
-            else if (array[i] > array[stat.high])
-                stat.high = i;
+        else if (array[i] > array[stat.high])
+            stat.high = i;
     }
 
     return stat;
 }
 
 double
-compute_average(int v[USERINPUT_MAX])
+compute_average(int *array, size_t arrayLen)
 {
     int sum;
 
-    for (int i = 0; i < USERINPUT_COUNT && VALUES[i] != USERINPUT_SENTINEL; ++i)
-        sum = sum + i;
+    for (int i = 0; i < arrayLen; ++i)
+        sum = sum + array[i];
 
-    return sum / USERINPUT_COUNT;
+    return sum / arrayLen;
 }
 
 // In this assignment normalization means each element in the original user-inputted array
 // minus the mean average
-int *compute_normalization(int v[USERINPUT_COUNT], float avg)
+int const *
+compute_normalization(int *array, float avg)
 {
-    int normalized[USERINPUT_COUNT + 1];
+    static int narray[USERINPUT_MAX];
 
-    for (int i = 0; i < USERINPUT_COUNT && v[i] != USERINPUT_SENTINEL; ++i)
-        normalized[i] = v[i - avg];
+    for (int i = 0; i < USERINPUT_MAX; ++i)
+        narray[i] = array[i] - avg;
 
-    return normalized; 
+    return narray; 
 }
 
 int
 main(int argc, char **argv)
 {
     static char const *promptstr = "VECRQ? ";
-    int USERINPUTS[USERINPUT_MAX + 1];
+    int array[USERINPUT_MAX] = {1, 2, 3, 4, 5};
+    int const *res = compute_normalization(array, 3.0);
+    struct lohistat stat = get_lohi(array, 6);
+    
+    printf("lowest %d highest %d", array[stat.low], array[stat.high]);
 
+    for (int i = 0; i < 5; ++i)
+        printf("norm >> " + "%d ", res[i]);
 
     return 0;
 }
